2f7fbf0aee8ac04cffccb9bcd8ad0bba72a7ff51
jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-03-27 10:31:48 +0530

    + RHS: Chat box, Emoji & Mic icon

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,5 +1,11 @@
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import {
+  AttachFile,
+  InsertEmoticon,
+  MoreVert,
+  SearchOutlined,
+} from "@material-ui/icons";
+import MicIcon from "@material-ui/icons/Mic";
 import React from "react";
 import "./Chat.css";
 import moment from "moment";
@@ -41,6 +47,15 @@ function Chat() {
             {moment().format("DD/MM/YY hh:mm A")}
           </span>
         </p>
+      </div>
+
+      <div className="chat_footer">
+        <InsertEmoticon />
+        <form>
+          <input placeholder="Type a message" type="text" />
+          <button type="submit">Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
