db8ce31145f479f647870c59343c7934ba41d604
jothinayagan <v.i.jothinayagan.307@gmail.com>
2021-03-27 10:36:45 +0530

    + Message icon changed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Sidebar.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
-import ChatIcon from "@material-ui/icons/Chat";
+import AddIcon from "@material-ui/icons/Add";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from "./SidebarChat";
@@ -17,7 +17,7 @@ function Sidebar() {
             <DonutLargeIcon />
           </IconButton>
           <IconButton>
-            <ChatIcon />
+            <AddIcon />
           </IconButton>
           <IconButton>
             <MoreVertIcon />
